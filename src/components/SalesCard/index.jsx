ee03a1cfe4e53b6e9e2411fdf6bbf311ff64bb62
pawan-perennial21 <95855958+pawan-perennial21@users.noreply.github.com>
2023-04-18 11:04:39 +0530

    1 file modified

--- src/components/SalesCard/index.jsx
+++ src/components/SalesCard/index.jsx
@@ -2,24 +2,31 @@ import { Card, Pagination, Typography } from 'antd';
 import React, { useState } from 'react';
 
 export const SalesCard = ({ el }) => {
+  const [saleProductData, setSaleProductData] = useState(el.products);
+  const onShowSizeChangeSales = (current) => {
+    console.log('current', current);
+    setSaleProductData(el.products.slice((current - 1) * 5, current * 5));
+  };
   return (
     <Card title={el.title} style={{ width: 700, marginBottom: '20px' }}>
-      {el.products.map((product) => (
+      {saleProductData.map((product) => (
         <div
           style={{
             display: 'flex',
             flexDirection: 'column',
             marginBottom: '10px',
           }}
+          key={product.productId}
         >
           <Typography.Text>{product.subject}</Typography.Text>
           <Typography.Text>{product.content}</Typography.Text>
         </div>
       ))}
       <Pagination
-        // defaultPageSize={5}
-        defaultValue={3}
-        hideOnSinglePage={el.products.length <= 5}
+        // showTotal={true}
+        defaultPageSize={5}
+        defaultValue={1}
+        onChange={onShowSizeChangeSales}
         total={el.products.length}
       />
     </Card>
